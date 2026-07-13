8cd379f775a71d110637736b6145768944f836fe
rajtripathi99 <rajj.tripathii@gmail.com>
2026-07-14 00:51:03 +0530

    add: navbar and hero added

---
+++ src/components/Hero.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Hero() {
+    return (
+        <section className="flex flex-col items-center pt-24 relative max-h-fit container">
+            <div className="flex flex-col gap-4 items-center">
+                <div className="flex items-center gap-1 px-2 py-1.5 bg-black border border-white/15 w-fit rounded-full"><span className="bg-[#9855FF] px-[4px] py-px text-black font-bold rounded-full text-xs">NEW</span><span className="text-[#9855FF] text-xs ">Latest integration just arrived</span></div>
+                <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-white to-[#B372CF] text-transparent bg-clip-text  text-center leading-[1.1]">
+                    Boost your <br /> rankings with AI.
+                </h1>
+                <p className="text-white max-w-md text-center">Elevate your site’s visibility effortlessly with AI, where smart technology meets user-friendly SEO tools.</p>
+                <div className="p-1 border border-white/20 rounded-lg">
+                    <button className="bg-white border border-white/35 rounded-md px-3 py-1 text-sm tracking-wide text-black cursor-pointer">Start for Free</button>
+                </div>
+                <div className="  w-full h-full absolute inset-0 -z-10">
+                    <Image src="/assets/circles-bg.svg" alt="bg-img" className="object-fill pt-56 [mask-image:linear-gradient(to_top,transparent_20%,black_90%)]" fill priority />
+                </div>
+            </div>
+            <Image src="/assets/app.png" alt="dashboard-img" className="pointer-events-none -mt-10 [mask-image:linear-gradient(to_bottom,black_30%,transparent_70%)]" width="1120" height="815" draggable={false} loading="lazy" />
+        </section>
+    );
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { ChevronDownIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+    return (
+        <header className="container bg-black/20 sticky top-0 backdrop-blur-md z-99 border-white/15 border-b h-18 flex items-center justify-between">
+            <nav className="max-w-4xl mx-auto flex items-center justify-between w-full">
+                <Link href="/" className="text-white text-2xl font-bold pointer-events-none">
+                    <Image src="/assets/nav-logo.svg" alt="logo" width={40} height={40} draggable={false} loading="lazy" />
+                </Link>
+                <div className="flex items-center gap-10 px-8 py-2 rounded-full border border-white/15 text-md">
+                    <Link href="/" className="text-white/60 flex items-center gap-1">
+                        Features <ChevronDownIcon className="w-4 h-4" />
+                    </Link>
+                    <Link href="/" className="text-white/60">
+                        Developers
+                    </Link>
+                    <Link href="/" className="text-white/60 flex items-center gap-1">
+                        Company <ChevronDownIcon className="w-4 h-4" />
+                    </Link>
+                    <Link href="/" className="text-white/60">
+                        Blog
+                    </Link>
+                    <Link href="/" className="text-white/60">
+                        Changelog
+                    </Link>
+                </div>
+                <div className="p-1 border border-white/20 rounded-lg">
+                    <button className="bg-[#8C45FF]/40 backdrop-blur-md border border-white/35 rounded-md px-3 py-1 text-sm tracking-wide text-white shadow-[inset_0_0_6px_3px_rgba(255,255,255,0.25)] cursor-pointer">Join Waitlist</button>
+                </div>
+            </nav>
+        </header>
+    );
+}
